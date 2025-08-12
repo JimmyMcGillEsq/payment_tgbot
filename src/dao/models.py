560fd473d4fdd6b063da378d348e301cb2e3addb
Jimmy McGill <f2mn1cj42@mozmail.com>
2025-08-12 17:02:03 +0300

    Create SQL engine

--- src/dao/models.py
+++ src/dao/models.py
@@ -1,8 +1,6 @@
-from email.policy import default
-
-from sqlalchemy import Column, Integer, String, Boolean, DateTime
-from sqlalchemy.ext.declarative import declarative_base
-from datetime import datetime
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, create_engine
+from sqlalchemy.orm import declarative_base
+from datetime import datetime, UTC
 
 # базовый класс.
 Base = declarative_base()
@@ -12,12 +10,15 @@ class User(Base):
 
     telegram_id = Column(Integer, primary_key=True) #ID пользователя в Телеграмм
     username = Column(String(50), nullable=True) #username(может быть None, если скрыт)
-    first_name = Column(String(50)), nullable=False) # имя(обязательное поле)
-    last_name = Column(String(50)), nullable=True) # фамилия(может быть None)
-    phone = Column(String(20)), nullable=True) # номер телефона(может быть None)
+    first_name = Column(String(50), nullable=False) #имя(обязательное поле)
+    last_name = Column(String(50), nullable=True) # фамилия(может быть None)
+    phone = Column(String(20), nullable=True) # номер телефона(может быть None)
     is_admin = Column(Boolean, default=False) # админ ли?
     is_active = Column(Boolean, default=True) # Active account?
-    registered_at = Column(DateTime, default=datetime.) # registration date
+    registered_at = Column(DateTime, default=datetime.now(tz=UTC)) # registration date
 
 def __repr__(self):
         return f'<User(id={self.telegram_id}, username="{self.username}")>'
+engine = create_engine(
+    os.getenv("PAYBOT_DATABASE_URL",'sqlite:///PaymentsBot.db'),
+    echo=True)
