eef67610e5f8632d10aa0037731825182eb1c81b
Jimmy McGill <f2mn1cj42@mozmail.com>
2025-08-13 15:21:25 +0300

    Replace Integer module with BigInteger. Use BigInteger type in telegram_id

--- src/dao/models.py
+++ src/dao/models.py
@@ -1,8 +1,9 @@
 import os
-
-from sqlalchemy import Column, Integer, String, Boolean, DateTime, create_engine
-from sqlalchemy.orm import declarative_base
 from datetime import datetime, UTC
+
+from sqlalchemy import Column, BigInteger, String, Boolean, DateTime, create_engine
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 # базовый класс.
 Base = declarative_base()
@@ -10,7 +11,7 @@ Base = declarative_base()
 class User(Base):
     __tablename__ = 'payment_users'
 
-    telegram_id = Column(Integer, primary_key=True) #ID пользователя в Телеграмм
+    telegram_id = Column(BigInteger, primary_key=True) #ID пользователя в Телеграмм
     username = Column(String(50), nullable=True) #username(может быть None, если скрыт)
     first_name = Column(String(50), nullable=False) #имя(обязательное поле)
     last_name = Column(String(50), nullable=True) # фамилия(может быть None)
@@ -21,6 +22,16 @@ class User(Base):
 
 def __repr__(self):
         return f'<User(id={self.telegram_id}, username="{self.username}")>'
+
+# Создаем движок SQL
 engine = create_engine(
     os.getenv("PAYBOT_DATABASE_URL",'sqlite:///PaymentsBot.db'),
     echo=True)
+
+# Создаем асинхронный движок SQ
+async_engine = create_async_engine(
+    os.getenv("PAYBOT_DATABASE_URL","sqlite+aiosqlite:///PaymentsBot.db"),
+    echo=True)
+
+#Создание асинхронных сессий для работе с БД
+AsyncSessionLocal = async_sessionmaker(async_engine, expire_on_commit=False)
