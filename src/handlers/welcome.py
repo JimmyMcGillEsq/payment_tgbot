45844341db10c46d4416927e8d2eaf218c63a3b7
Jimmy McGill <f2mn1cj42@mozmail.com>
2025-08-13 15:22:23 +0300

    Add registration after command /start

--- src/handlers/welcome.py
+++ src/handlers/welcome.py
@@ -1,7 +1,20 @@
 from src.common import bot
+from src.dao.models import AsyncSessionLocal, User
 
-# Handle '/start' and '/help'
-@bot.message_handler(commands=['help', 'start'])
+# Handle '/start'
+@bot.message_handler(commands=['start'])
 async def send_welcome(message):
-    text = 'Hi, I am EchoBot.\nJust write me something and I will repeat it!'
-    await bot.reply_to(message, text)
+    async with AsyncSessionLocal() as session:
+        user =  await session.get(User, message.from_user.id)
+        if not user:
+            user = User(
+                telegram_id=message.from_user.id,
+                username=message.from_user.username,
+                first_name=message.from_user.first_name,
+                last_name=message.from_user.last_name
+            )
+            session.add(user)
+            await session.commit()
+            await bot.reply_to(message, 'Вы успешно зарегистрированы')
+        else:
+            await bot.send_message(message.chat.id, "С возвращением")
